5d91cc896db060929c4b82528e6ca78d23981923
Akira Taguchi <akirahattivatti@gmail.com>
2023-10-13 08:48:00 +0300

    Complete 2.15

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,10 +1,7 @@
 class Beer < ApplicationRecord
+	include RatingAverage
 	belongs_to :brewery
 	has_many :ratings, dependent: :destroy
-
-	def average_rating
-		ratings.average(:score)
-	end
 
 	def to_s
 		name + ", " + brewery.name
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,15 +1,13 @@
 class Brewery < ApplicationRecord
+	include RatingAverage
 	has_many :beers, dependent: :destroy
 	has_many :ratings, through: :beers
+
 	def print_report
 		puts name
     puts "established at year #{year}"
     puts "number of beers #{beers.count}"
   end
-
-	def average_rating
-		ratings.average(:score)
-	end
 
 	def restart
 		self.year = 2022
