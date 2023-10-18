66f30f4e41f1a11c7160e1e8fd10485a494bb301
Akira Taguchi <akirahattivatti@gmail.com>
2023-10-18 09:20:11 +0300

    Complete 3.4

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 	root 'breweries#index'
   get 'signup', to: 'users#new'
 	get 'all_beers', to: 'beers#index'
+	get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   resource :session, only: [:new, :create, :destroy]
 end
