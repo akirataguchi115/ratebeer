bc23354387e1dc5011b9b0b0022151ca341169d3
Akira Taguchi <akirahattivatti@gmail.com>
2023-10-23 14:08:52 +0300

    Complete 3.6

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,8 +10,10 @@ class RatingsController < ApplicationController
 
 	def create
 		rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+		rating.user = current_user
+		rating.save
 		session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-		redirect_to ratings_path
+		redirect_to current_user
 	end
 
 	def destroy
