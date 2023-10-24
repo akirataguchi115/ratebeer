269938871b5d46265d4912d23bec458991f82ee8
Akira Taguchi <akirahattivatti@gmail.com>
2023-10-24 08:58:48 +0300

    Complete 3.7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-    # render the signing up page
-  end
+	def new
+			# render the signing up page
+	end
 
-  def create
-    # retrieves from the database the user that matches the username
-    user = User.find_by username: params[:username]
-    # saves the user ID who signed up (if the user exists)
-    session[:user_id] = user.id if user
-    # redirects the user to their own page
-    redirect_to user
-  end
+	def create
+		user = User.find_by username: params[:username]
+		if user.nil?
+			redirect_to signin_path, notice: "User #{params[:username]} does not exist!"
+		else
+			session[:user_id] = user.id
+			redirect_to user, notice: "Welcome back!"
+		end
+	end
 
-  def destroy
-    # resets the session
-    session[:user_id] = nil
-    # redirects the application to the main page
-    redirect_to :root
-  end
+	def destroy
+		# resets the session
+		session[:user_id] = nil
+		# redirects the application to the main page
+		redirect_to :root
+	end
 end
