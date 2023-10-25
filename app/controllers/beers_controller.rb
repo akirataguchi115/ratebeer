6d214c4ea97992e2731eb92ae3af3b2aeaee0f75
Akira Taguchi <akirahattivatti@gmail.com>
2023-10-25 14:01:42 +0300

    Complete 3.8

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,74 +1,76 @@
 class BeersController < ApplicationController
-  before_action :set_beer, only: %i[show edit update destroy]
+	before_action :set_beer, only: %i[show edit update destroy]
 
-  # GET /beers or /beers.json
-  def index
-    @beers = Beer.all
-  end
+	# GET /beers or /beers.json
+	def index
+		@beers = Beer.all
+	end
 
-  # GET /beers/1 or /beers/1.json
-  def show
-  end
+	# GET /beers/1 or /beers/1.json
+	def show
+	end
 
-  # GET /beers/new
-  def new
-    @beer = Beer.new
-    @breweries = Brewery.all
-    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Low alcohol"]
-  end
+	# GET /beers/new
+	def new
+		@beer = Beer.new
+		@breweries = Brewery.all
+		@styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Low alcohol"]
+	end
 
-  # GET /beers/1/edit
-  def edit
-    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Low alcohol"]
-  end
+	# GET /beers/1/edit
+	def edit
+		@styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Low alcohol"]
+	end
 
-  # POST /beers or /beers.json
-  def create
-    @beer = Beer.new(beer_params)
+	# POST /beers or /beers.json
+	def create
+		@beer = Beer.new(beer_params)
 
-    respond_to do |format|
-      if @beer.save
-        format.html { redirect_to beers_path, notice: "Beer was successfully created." }
-        format.json { render :show, status: :created, location: @beer }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @beer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @beer.save
+				format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
+				format.json { render :show, status: :created, location: @beer }
+			else
+				@breweries = Brewery.all
+				@styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Lowalcohol"]
+				format.html { render :new }
+				format.json { render json: @beer.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /beers/1 or /beers/1.json
-  def update
-    respond_to do |format|
-      if @beer.update(beer_params)
-        format.html { redirect_to beers_path, notice: "Beer was successfully updated." }
-        format.json { render :show, status: :ok, location: @beer }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @beer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PATCH/PUT /beers/1 or /beers/1.json
+	def update
+		respond_to do |format|
+			if @beer.update(beer_params)
+				format.html { redirect_to beers_path, notice: "Beer was successfully updated." }
+				format.json { render :show, status: :ok, location: @beer }
+			else
+				format.html { render :edit, status: :unprocessable_entity }
+				format.json { render json: @beer.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /beers/1 or /beers/1.json
-  def destroy
-    @beer.destroy
+	# DELETE /beers/1 or /beers/1.json
+	def destroy
+		@beer.destroy
 
-    respond_to do |format|
-      format.html { redirect_to beers_url, notice: "Beer was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+		respond_to do |format|
+			format.html { redirect_to beers_url, notice: "Beer was successfully destroyed." }
+			format.json { head :no_content }
+		end
+	end
 
-  private
+	private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_beer
-    @beer = Beer.find(params[:id])
-  end
+	# Use callbacks to share common setup or constraints between actions.
+	def set_beer
+		@beer = Beer.find(params[:id])
+	end
 
-  # Only allow a list of trusted parameters through.
-  def beer_params
-    params.require(:beer).permit(:name, :style, :brewery_id)
-  end
+	# Only allow a list of trusted parameters through.
+	def beer_params
+		params.require(:beer).permit(:name, :style, :brewery_id)
+	end
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,19 +1,19 @@
 class Brewery < ApplicationRecord
-	 include RatingAverage
-	 has_many :beers, dependent: :destroy
-	 has_many :ratings, through: :beers
+	include RatingAverage
+	has_many :beers, dependent: :destroy
+	has_many :ratings, through: :beers
 
-	 validates :name, presence: true
-	 validates :year, numericality: { greater_than_or_equal_to: 1040, less_than_or_equal_to: 2022, only_integer: true }
+	validates :name, presence: true
+	validates :year, numericality: { greater_than_or_equal_to: 1040, less_than_or_equal_to: ->{ Date.current.year }.call, only_integer: true }
 
-	 def print_report
- 		 puts name
-    puts "established at year #{year}"
-    puts "number of beers #{beers.count}"
-  end
+	def print_report
+		puts name
+		puts "established at year #{year}"
+		puts "number of beers #{beers.count}"
+	end
 
-	 def restart
- 		 self.year = 2022
- 		 puts "changed year to #{year}"
- 	end
+	def restart
+ 		self.year = 2022
+ 		puts "changed year to #{year}"
+	end
 end
