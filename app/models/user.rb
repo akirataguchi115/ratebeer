adff3ff10c2a3f0345098494fcf4350baa014fc8
Akira Taguchi <akirahattivatti@gmail.com>
2023-12-19 10:49:14 +0200

    Complete 4.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,10 @@ class User < ApplicationRecord
 
 		ratings.order(score: :desc).limit(1).first.beer
 	end
+
+	def favorite_style
+		return nil if ratings.empty?
+
+		ratings.order(score: :desc).limit(1).first.beer.style
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	def create_beer_with_rating(object, score)
+	def create_beer_with_rating(object, score, style)
 		beer = FactoryBot.create(:beer)
+		if style != "" then
+			beer.style = style
+		end
+
 		FactoryBot.create(:rating, beer: beer, score: score, user: object[:user])
 		beer
 	end
 
 	def create_beers_with_many_ratings(object, *scores)
 		scores.each do |score|
-			create_beer_with_rating(object, score)
+			create_beer_with_rating(object, score, "")
 		end
 	end
 
@@ -63,8 +67,26 @@ RSpec.describe User, type: :model do
 
 		it "is the one with highest rating if several rated" do
 			create_beers_with_many_ratings({user: user}, 10, 20, 15, 7, 9)
-			best = create_beer_with_rating({user: user}, 25)
+			best = create_beer_with_rating({user: user}, 25, "")
 			expect(user.favorite_beer).to eq(best)
+		end
+	end
+
+	describe "favorite style" do
+		let(:user){ FactoryBot.create(:user) }
+
+		it "has method for determining one" do
+			expect(user).to respond_to(:favorite_style)
+		end
+
+		it "without ratings does not have one" do
+			expect(user.favorite_style).to eq(nil)
+		end
+
+		it "is the one with highest rating if several rated" do
+			create_beers_with_many_ratings({user: user}, 10, 20, 15, 7,9)
+			best = create_beer_with_rating({user: user}, 25, "IPA")
+			expect(user.favorite_style == (best.style))
 		end
 	end
 end
